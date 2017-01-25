8440eea160eedfc3352611290535ab50cde52c64
田中賢治 <key.m4096.kung.fu@gmail.com>
2017-01-25 11:32:09 +0900

    不要な箇所を削除

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,14 +3,6 @@ import JSON
 
 let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
-}
-
-drop.resource("posts", PostController())
-
 drop.get("json") { request in
     let json = try JSON(node: [
         "name": "Tanaka Kenji",
